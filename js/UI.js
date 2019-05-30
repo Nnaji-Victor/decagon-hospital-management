704d974ded6ef68466fc71e28dc3fec0f78d9e02
Nnaji Victor <nnajivictor0@gmail.com>
2019-05-30 23:23:55 +0100

    database changes

--- js/UI.js
+++ js/UI.js
@@ -1,24 +1,27 @@
 class UI {
 
-        displayMessage(message, classname) {
-                const div = document.createElement('div');
-                div.innerHTML = ` 
-                <div class="alert alert-${classname} alert-dismissible fade show" role="alert">
-                <strong>${message}
-                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                  <span aria-hidden="true">&times;</span>
-                </button>
-              </div>
-                `;
+    displayMessage(message, classname) {
+      const div = document.createElement('div');
+      div.innerHTML = ` 
+      <div class="alert alert-${classname} alert-dismissible fade show" role="alert">
+      <strong>${message}
+      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </div>
+      `;
 
-                const reference = document.querySelector('#ref');
-                const parentNode = reference.parentElement;
+      const reference = document.querySelector('#ref');
+      const parentNode = reference.parentElement;
 
-                parentNode.insertBefore(div, ref);
+      parentNode.insertBefore(div, ref);
 
-                setTimeout(()=>{
-                 document.querySelector('.alert').remove();
-                },3000)
-        }
+      setTimeout(()=>{
+        document.querySelector('.alert').remove();
+      },3000)
+    }
+
+    
+
 }
 
